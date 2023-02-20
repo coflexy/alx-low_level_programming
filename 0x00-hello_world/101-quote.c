3c1b19a24c0a1aad6a40680cf2fcf7903d7bee52
Collins <coflexy@gmail.com>
2023-02-20 09:31:24 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
- *
- * Return: Always 1 (Success)
+ * main - A c program that prints a line to the standard error
+ * Return 1 (Success)
  */
 int main(void)
 {
