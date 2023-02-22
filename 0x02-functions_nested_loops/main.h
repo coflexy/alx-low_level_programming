404215d4d193880ede940f71c57c2e28df7d91b5
Collins <coflexy@gmail.com>
2023-02-22 02:24:36 -0800

    putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+Prototype: void print_alphabet(void);
+Prototype: void print_alphabet_x10(void);
+Prototype: int _islower(int c);
+Prototype: int _isalpha(int c);
+Prototype: int print_sign(int n);
+Prototype: int _abs(int);
+Prototype: int print_last_digit(int);
+Prototype: void jack_bauer(void);
+Prototype: void times_table(void);
+Prototype: int add(int, int);
+Prototype: void print_to_98(int n);
+Prototype: void print_times_table(int n);
