105cc2d85041da1d25361d1a67f8fd629033c4f5
Collins <coflexy@gmail.com>
2023-02-28 14:20:00 -0800

    all

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strlen - returns the lenght of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int longi = 0;
+
+	while (*s l = '\0')
+	{
+		longi++;
+		s++;
+	}
+
+	return (longi);
+}
+
+
